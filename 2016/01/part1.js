7935a1769ae432ffe52a871643a4dbde2deb981d
Will Rigby-Hall <will@wgrh.io>
2016-12-01 19:27:16 -0500

    1.1 done

---
+++ 2016/01/part1.js
@@ -0,0 +1,49 @@
+module.exports = (input) => {
+
+  input = 'R5, L5, R5, R3';
+  // parse the input data
+  var data = input.split(', ').map((s) => {
+    // convert R and L turns to positive and negative
+    if (s[0] === 'R') {
+      return [1, parseInt(s.slice(1))];
+    } else {
+      return [-1, parseInt(s.slice(1))];
+    }
+  });
+
+  // initial position and direction
+  var coords = [0, 0]
+  var direction = 1;
+
+  // loop through instructions and modify coordinates
+  for (let i = 0; i < data.length; i++) {
+    // use alternation to decide direction and coordinate to modify
+    if (i % 2 === 0) {
+      direction = (direction === data[i][0]) ? 1 : -1;
+    } else {
+      direction = (direction !== data[i][0]) ? 1 : -1;
+    }
+    
+    coords[i % 2] += data[i][1] * direction;
+  }
+
+  return Math.abs(coords[0]) + Math.abs(coords[1]);
+}
+
+/*
+
+increment y
+1) -----> L = + - => +
+2) -----> R = + + => -
+3) <----- L = - - => -
+4) <----- R = - + => +
+
+increment x
+5) ^^^^^^ L = + - => -
+6) ^^^^^^ R = + + => +
+7) vvvvvv L = - - => +
+8) vvvvvv R = - + => -
+
+
+
+*/
--- run.js
+++ run.js
@@ -2,18 +2,18 @@ const moment = require('moment');
 const fs = require('fs');
 
 module.exports = function run(year, day, part) {
-  let input = fs.readFileSync(`./${year}/day${day}/input`, 'utf-8').trimRight().split('\n');
+  let input = fs.readFileSync(`./${year}/${day}/input.txt`, 'utf-8').trimRight().split('\n');
   let startTime = moment();
 
   console.log(`${year}.${day}.${part} running...`);
 
-  let answer = require(`./${year}/day${day}/part${part}`)(input.length === 1 ? input[0] : input);
+  let answer = require(`./${year}/${day}/part${part}`)(input.length === 1 ? input[0] : input);
 
-  console.log(`${year}.${day}.${part} answer:\t`, answer);
+  console.log(`${year}.${day}.${part} answer: `, answer);
 
   let endTime = moment();
   
-  console.log(`${year}.${day}.${part} time:\t`, moment(endTime.diff(startTime)).format('mm:ss:SSSS'));
+  console.log(`${year}.${day}.${part} time: `, moment(endTime.diff(startTime)).format('mm:ss:SSSS'));
 
   return answer;
 }

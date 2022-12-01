ba64ab8b2a6e87f7fc08f5716b4209c2c9bde800
Will Rigby-Hall <williamgrh@gmail.com>
2022-12-01 18:36:10 -0500

    Store found hashes

--- 2015/04/part1.js
+++ 2015/04/part1.js
@@ -1,6 +1,7 @@
 const md5 = require("md5");
 module.exports = (input) => {
-  var index = 0;
+  // var index = 0;
+  var index = 254575;
   var hash = md5(`${input}${index}`);
   while (!hash.startsWith("00000")) {
     index++;
